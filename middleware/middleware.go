9032a28a8acf43580d568782881d9f5d18e784d9
jperkelens <jperkelens@gmail.com>
2016-06-21 16:54:09 -0400

    Adds Checksum header

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"crypto/sha1"
+	"encoding/hex"
 	"flag"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"net/http/httptest"
+	"sort"
 	"strconv"
+	"strings"
 )
 
 const (
@@ -15,7 +22,39 @@ const (
 
 func ChecksumMiddleware(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// your code goes here ...
+		// Record response
+		res := httptest.NewRecorder()
+		h.ServeHTTP(res, r)
+		hash := sha1.New()
+
+		// Read headers + body
+		var headerKeys = []string{}
+		var headers = res.Header()
+		var body, _ = ioutil.ReadAll(res.Body)
+
+		// Sort Headers
+		for k, _ := range headers {
+			headerKeys = append(headerKeys, k)
+		}
+		sort.Strings(headerKeys)
+
+		// Add Code
+		io.WriteString(hash, fmt.Sprintf("%v", res.Code)+crlf)
+
+		// Append Headers
+		for _, header := range headerKeys {
+			io.WriteString(hash, header+colonspace+
+				strings.Join(headers[header], "")+crlf)
+		}
+
+		io.WriteString(hash, "X-Checksum-Headers: "+
+			strings.Join(headerKeys, ";")+crlf+crlf+string(body))
+
+		// Add Checksum header
+		checksum := hash.Sum(nil)
+		w.Header().Set("X-Checksum", hex.EncodeToString(checksum))
+
+		h.ServeHTTP(w, r)
 	})
 }
 
